7b97c4839611af17d882074fae34d4b2f15fb9d3
aletheiaaaaa <carbon674@gmail.com>
2026-04-17 17:42:00 +0530

    test

--- deep_sae/__main__.py
+++ deep_sae/__main__.py
@@ -20,9 +20,9 @@ def make_parser() -> argparse.ArgumentParser:
     parser.add_argument("--k_feat", default=72, type=int)
     parser.add_argument("--batches_to_dead", default=1000000, type=int)
 
-    parser.add_argument("--lr", default=1e-5, type=float)
+    parser.add_argument("--lr", default=1e-4, type=float)
     parser.add_argument("--batch_size", default=64, type=int)
-    parser.add_argument("--frac_inactive", default=0.5, type=float)
+    parser.add_argument("--frac_inactive", default=0.0, type=float)
     parser.add_argument("--upload_every", default=128, type=int)
     parser.add_argument("--save_path", default="./sae", type=str)
 
